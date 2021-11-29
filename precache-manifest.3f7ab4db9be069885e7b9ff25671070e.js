70a83f15c7e8029ec99b8ff4d4d8b06388c241f2
vananz123 <88036322+vananz123@users.noreply.github.com>
2021-11-29 10:51:58 +0700

    Updates

---
+++ precache-manifest.3f7ab4db9be069885e7b9ff25671070e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b3efe69ecd4b07f9c37d6bbce98a982",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce7c7507bfad4716c3f7",
+    "url": "/static/css/2.ad76a385.chunk.css"
+  },
+  {
+    "revision": "f5952b5eb13d0d83570f",
+    "url": "/static/css/main.1930e7a3.chunk.css"
+  },
+  {
+    "revision": "ce7c7507bfad4716c3f7",
+    "url": "/static/js/2.fee1d04e.chunk.js"
+  },
+  {
+    "revision": "2182b49f32ec797f3b9c375cbe5c7fb3",
+    "url": "/static/js/2.fee1d04e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5952b5eb13d0d83570f",
+    "url": "/static/js/main.b4908101.chunk.js"
+  },
+  {
+    "revision": "0e5dc210f421aea4b19a",
+    "url": "/static/js/runtime-main.7aeb85a8.js"
+  }
+]);
